9b18d1290dae524e2f10758fa6a3736377f1362a
Kiki Ramdani <amandaazhar8@gmail.com>
2025-07-28 16:49:41 +0700

    flutter

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:latihan_flutter/main_layout.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: HelloFultter()
+    );
+  }
+}
+
+class HelloFultter extends StatelessWidget {
+  const HelloFultter({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: 'Belajar Flutter',
+      body: Center(
+        child: Text('Hello, Flutter',
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            backgroundColor: Colors.blueAccent,
+          ),
+        ),
+      ),
+    );
+  }
+}
